15cb40eaa0180c11d3e5f68edcec9d460b16fad3
Maheshlucky84 <168493172+Maheshlucky84@users.noreply.github.com>
2024-04-30 08:36:03 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "23674419"
 api_hash = "27e298d43a54d175823872d78e5e9295"
-bot_token = "6806457016:AAGQnuwVH-Ab3d_rB857nC3roMiDH5IZAcA"
+bot_token = "6851459979:AAFofn-uJHFL1eMklh-GTqPB82Rd5CQ2iR8"
